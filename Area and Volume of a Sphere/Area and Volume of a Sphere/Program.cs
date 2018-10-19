2349423b8d521bd656757569e44aa42aac8fd7c2
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-19 22:01:58 +0300

    Add files via upload

---
+++ Area and Volume of a Sphere/Area and Volume of a Sphere/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_and_Volume_of_a_Sphere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double küre_yarıçapı = 0, alan = 0, hacim = 0;
+            Console.WriteLine("Kürenin Yarıçapını Giriniz = ");
+            küre_yarıçapı = Convert.ToDouble(Console.ReadLine());
+
+            alan = 4 * Math.PI * (Math.Pow(küre_yarıçapı, 2));
+            hacim = (4 / 3) * Math.PI * (Math.Pow(küre_yarıçapı, 3));
+
+            Console.WriteLine("Kürenin Alanı = " + alan);
+            Console.WriteLine("Kürenin Hacmi = " + hacim);
+
+
+        }
+    }
+}
